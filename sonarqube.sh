a98d1fd772473fce035d0da9c93ce946d6c4344d
Thiago Barradas <thiagobrowskt@gmail.com>
2019-04-26 02:41:18 -0300

    :art: try status check with sonar

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 if [[ -z "${APPVEYOR_PULL_REQUEST_NUMBER}" ]]; then
   dotnet sonarscanner begin /o:thiagobarradas-github /k:ThiagoBarradas_jsonmasking /v:${version} /d:sonar.host.url=https://sonarcloud.io /d:sonar.login=${SONARQUBE_TOKEN} /d:sonar.cs.opencover.reportsPaths="opencover.xml" /d:sonar.branch.name=${APPVEYOR_REPO_BRANCH} 
 else 
-  dotnet sonarscanner begin /o:thiagobarradas-github /k:ThiagoBarradas_jsonmasking /v:${version} /d:sonar.host.url=https://sonarcloud.io /d:sonar.login=${SONARQUBE_TOKEN} /d:sonar.cs.opencover.reportsPaths="opencover.xml" /d:sonar.pullrequest.key=${APPVEYOR_PULL_REQUEST_NUMBER} /d:sonar.branch.name=${APPVEYOR_REPO_BRANCH} /d:sonar.pullrequest.branch=${APPVEYOR_REPO_BRANCH} /d:sonar.pullrequest.base=${APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH} 
+  dotnet sonarscanner begin /o:thiagobarradas-github /k:ThiagoBarradas_jsonmasking /v:${version} /d:sonar.host.url=https://sonarcloud.io /d:sonar.login=${SONARQUBE_TOKEN} /d:sonar.cs.opencover.reportsPaths="opencover.xml" /d:sonar.pullrequest.key=${APPVEYOR_PULL_REQUEST_NUMBER} /d:sonar.pullrequest.branch=${APPVEYOR_REPO_BRANCH} /d:sonar.pullrequest.base=${APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH} 
 fi 
